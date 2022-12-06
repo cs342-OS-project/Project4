e700c1dac9cc56cbb951dc78cc6637a987850974
Efe Erkan <72245656+EfeErkan@users.noreply.github.com>
2022-12-06 19:24:03 +0300

    diskprint created

---
+++ diskprint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define FILE_NAME_SIZE 64
+#define BLOCK_SIZE 4096
+
+int main(int argc, char const *argv[])
+{
+    char devname[FILE_NAME_SIZE];  
+    int fd; 
+    int n; 
+    off_t offset; 
+    int blocknum;  
+    unsigned char buffer[BLOCK_SIZE];
+
+    strcpy(devname, argv[1]);
+
+    fd = open (devname, O_RDONLY);
+
+    if (fd < 0) 
+    { 
+        printf ("can not open disk file\n"); 
+        exit(1); 
+    } 
+
+    printf("Disk opened\n");
+    
+    return 0;
+}
