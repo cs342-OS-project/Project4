f7890f6b1a2125fe5ec055b779b67f391a8823cb
Efe Erkan <72245656+EfeErkan@users.noreply.github.com>
2022-12-06 20:26:08 +0300

    Update diskprint.c

--- diskprint.c
+++ diskprint.c
@@ -7,6 +7,8 @@
 #define FILE_NAME_SIZE 64
 #define BLOCK_SIZE 4096
 
+#define SUPERBLOCK_OFFSET 1024
+
 int main(int argc, char const *argv[])
 {
     char devname[FILE_NAME_SIZE];  
@@ -22,11 +24,32 @@ int main(int argc, char const *argv[])
 
     if (fd < 0) 
     { 
-        printf ("can not open disk file\n"); 
-        exit(1); 
+        printf ("can not open disk file\n");
+        exit(1);
     } 
 
-    printf("Disk opened\n");
+    // SUPER BLOCK
+    printf("Super Block Information\n");
+
+    lseek(fd, 1024, SEEK_SET);
+
+    n = read(fd, buffer, BLOCK_SIZE);
+
+    if (n == BLOCK_SIZE) 
+    {
+        char number[4];
+        for (int i = 0; i < 4; i++)
+        {
+            number[i] = buffer[i];
+            printf("%c\n", number[i]);
+        }
+
+        
+    }
+    else
+    {
+        printf("Error\n");
+    }
 
     return 0;
 }
